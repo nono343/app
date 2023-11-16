783521e99cb50cec7056f7bb9e1076b722b0feae
nono343 <ags1305@gmail.com>
2023-11-16 07:02:37 +0100

    v1.0.2

--- src/components/adminCategory.js
+++ src/components/adminCategory.js
@@ -499,7 +499,7 @@ function AdminCategory() {
                 ))}
               </select>
             </div>
-            
+
             <div>
               <label htmlFor="monthSelector" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                 Meses de producción:
@@ -530,8 +530,8 @@ function AdminCategory() {
           </form>
 
           <div className='flex justify-center mt-5'>
-              <button onClick={handleUploadProduct} type="button" class="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Crear Producto</button>
-            </div>
+            <button onClick={handleAgregarMeses} type="button" class="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Añadir meses</button>
+          </div>
 
 
           <div className="max-w-screen-xl mx-auto overflow-x-auto">
